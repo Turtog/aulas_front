f51f2dbcaf2f88f82e816018eb29b529408a9a92
aluno <joaonunes.pl018@academico.ifsul.edu.br>
2025-04-15 09:45:41 -0300

    Atualizando atividade 2.1

--- atividade02/atividade2.2/src/components/CardImc/CardImc.jsx
+++ atividade02/atividade2.2/src/components/CardImc/CardImc.jsx
@@ -2,20 +2,49 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
 import './cards.css';
+import './style.css';
 
 export default function CardImc({ pessoa }) {
   const [peso, setPeso] = useState(pessoa.peso);
   const alt = pessoa.altura;
-  const [imc, setImc] = useState((peso / alt ** 2).toFixed(2));
+  const [imc, setImc] = useState((peso / alt ** 2));
+  const [cardClass, setCardClass] = useState('imcCard');
+
+  const incrementaPeso = () => {
+    setPeso((peso) => {
+      let novo_peso = peso + 1;
+      setImc(novo_peso / alt ** 2);
+      return novo_peso;
+    });
+  };
+
+  const decrementaPeso = () => {
+    setPeso((peso) => {
+      let novo_peso = peso - 1;
+      setImc(novo_peso / alt ** 2);
+      return novo_peso;
+    });
+  };
+
+  useEffect(() => {
+    if (imc < 24.5) 
+      setCardClass('imcCard imcGreen');
+    else if (imc > 24.5 && imc <= 30) 
+      setCardClass('imcCard imcYellow');
+    else
+      setCardClass('imcCard imcRed');
+  })
 
 
   return (
-    <div className={imc < 24.5  ? 'imcCard imcGreen' : imc >= 24.5 && imc <= 30 ? 'imcCard imcYellow' 
-      : imc > 30 ? 'imcCard imcRed': 'imcCard'}>
+    <div className={cardClass}>
       <h1>{pessoa.name}:</h1>
       <p>Altura: {alt} m</p>
-      <p>Peso: {peso}</p>
-      <p>Imc: {imc}</p>
+      <p>Peso: {peso}
+      <span onClick={incrementaPeso}>&nbsp;+&nbsp;</span>
+      <span onClick={decrementaPeso}>&nbsp;-&nbsp;</span>
+      </p>
+      <p>Imc: {imc.toFixed(2)}</p>
     </div>
   );
 }
